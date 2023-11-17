102f39265fc30d4c6cd10aa1b0aa818121f5c819
faildeny <grzegorz.skorupko@gmail.com>
2023-11-17 17:00:44 +0100

    Update resize script

--- resize.py
+++ resize.py
@@ -2,8 +2,9 @@ import os
 import cv2
 from tqdm import tqdm
 
-directory = "training/sa_1ch_debug"
+directory = "training/stacked_EDES"
 subdirs = ["source", "target"]
+
 
 target_size = 512
 
@@ -11,11 +12,16 @@ counter = 0
 
 for image_dir in subdirs:
     path = os.path.join(directory, image_dir)
+    output_path = os.path.join(directory + "_resized", image_dir)
+    os.makedirs(output_path, exist_ok=True)
     for filename in tqdm(os.listdir(path)):
         if filename.endswith(".png") or filename.endswith(".jpg"):
             img = cv2.imread(os.path.join(path, filename))
-            img = cv2.resize(img, (target_size, target_size), interpolation=cv2.INTER_AREA)
-            cv2.imwrite(os.path.join(path, filename), img)
+            img = cv2.resize(img, (target_size, target_size), interpolation=cv2.INTER_NEAREST)
+            cv2.imwrite(os.path.join(output_path, filename), img)
             counter += 1
+
+prompt_path = os.path.join(directory + "_resized", "prompt.json")
+os.copy(prompt_path, os.path.join(directory + "_resized", "prompt.json"))
 
 print("Resized ", counter, " images.")
