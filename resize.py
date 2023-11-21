98c631a14b0ebf5b05202b1fb75982573220c5fb
faildeny <grzegorz.skorupko@gmail.com>
2023-11-21 16:29:01 +0100

    Fix resize script

--- resize.py
+++ resize.py
@@ -6,13 +6,13 @@ directory = "training/stacked_EDES"
 subdirs = ["source", "target"]
 
 
-target_size = 512
+target_size = 128
 
 counter = 0
 
 for image_dir in subdirs:
     path = os.path.join(directory, image_dir)
-    output_path = os.path.join(directory + "_resized", image_dir)
+    output_path = os.path.join(directory + "_resized_" + target_size, image_dir)
     os.makedirs(output_path, exist_ok=True)
     for filename in tqdm(os.listdir(path)):
         if filename.endswith(".png") or filename.endswith(".jpg"):
@@ -21,7 +21,7 @@ for image_dir in subdirs:
             cv2.imwrite(os.path.join(output_path, filename), img)
             counter += 1
 
-prompt_path = os.path.join(directory + "_resized", "prompt.json")
-os.copy(prompt_path, os.path.join(directory + "_resized", "prompt.json"))
+prompt_path = os.path.join(directory + "_resized_" + target_size, "prompt.json")
+os.copy(prompt_path, os.path.join(directory + "_resized_" + target_size, "prompt.json"))
 
 print("Resized ", counter, " images.")
