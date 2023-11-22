85c282045826cf6af240c131c48b17ed68e0f56e
faildeny <grzegorz.skorupko@gmail.com>
2023-11-22 16:17:38 +0100

    Fix resize and interpolation modes

--- resize.py
+++ resize.py
@@ -6,22 +6,28 @@ directory = "training/stacked_EDES"
 subdirs = ["source", "target"]
 
 
-target_size = 128
+target_size = 512
 
 counter = 0
 
 for image_dir in subdirs:
     path = os.path.join(directory, image_dir)
-    output_path = os.path.join(directory + "_resized_" + target_size, image_dir)
+    out_directory = os.path.join(directory + "_resized_" + str(target_size))
+    output_path = os.path.join(out_directory, image_dir)
     os.makedirs(output_path, exist_ok=True)
+    if image_dir == "target":
+        interpolation_method = cv2.INTER_AREA
+    else:  
+        interpolation_method = cv2.INTER_NEAREST
+
     for filename in tqdm(os.listdir(path)):
         if filename.endswith(".png") or filename.endswith(".jpg"):
             img = cv2.imread(os.path.join(path, filename))
-            img = cv2.resize(img, (target_size, target_size), interpolation=cv2.INTER_NEAREST)
+            img = cv2.resize(img, (target_size, target_size), interpolation=interpolation_method)
             cv2.imwrite(os.path.join(output_path, filename), img)
             counter += 1
 
-prompt_path = os.path.join(directory + "_resized_" + target_size, "prompt.json")
-os.copy(prompt_path, os.path.join(directory + "_resized_" + target_size, "prompt.json"))
+prompt_path = os.path.join(out_directory, "prompt.json")
+os.copy(prompt_path, os.path.join(out_directory, "prompt.json"))
 
 print("Resized ", counter, " images.")
