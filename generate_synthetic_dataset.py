30847c0e8e90f6643b08d47ebbc13cf75504b0b0
faildeny <grzegorz.skorupko@gmail.com>
2024-01-05 13:14:55 +0100

    Add methods for synthetic dataset generation

--- generate_synthetic_dataset.py
+++ generate_synthetic_dataset.py
@@ -6,7 +6,6 @@ from pathlib import Path
 from tqdm import tqdm
 import cv2
 import einops
-import gradio as gr
 import numpy as np
 import torch
 import random
@@ -14,7 +13,6 @@ from torch.utils.data import DataLoader
 from tutorial_dataset import MyDataset
 
 from pytorch_lightning import seed_everything
-from annotator.util import resize_image, HWC3
 from cldm.model import create_model, load_state_dict
 from cldm.ddim_hacked import DDIMSampler
 
@@ -25,6 +23,7 @@ source_dataset_path = "./training/stacked_EDES_resized_128"
 model_checkpoint = 'lightning_logs/version_23/checkpoints/epoch=3-step=121187.ckpt' # 512
 # model_checkpoint = 'lightning_logs/version_43/checkpoints/epoch=18-step=71971.ckpt' # 128 
 
+# Features for random prompt generation
 sex = ['Male', 'Female']
 age = ['age in 50s', 'age in 60s', 'age in 70s', 'age in 80s', 'age in 90s']
 bmi = ['normal BMI', 'overweight BMI', 'obese BMI']
@@ -32,24 +31,41 @@ diagnosis = ['healthy', 'heart failure']
 
 features = [sex, age, bmi, diagnosis]
 
+# Initialize model
 model = create_model('./models/cldm_v21.yaml').cuda()
 model.load_state_dict(load_state_dict(model_checkpoint, location='cpu'))
 model = model.cuda()
 ddim_sampler = DDIMSampler(model)
 
-feature_to_find = "heart failure"
 dataset = MyDataset()
-diagnosed_masks = []
-for sample in dataset:
-    prompt = sample['txt']
-    if feature_to_find in prompt:
-        diagnosed_masks.append(sample['hint'])
 
-    # if len(diagnosed_masks) >= 10:
-    #     break
+# Load paths to masks with diagnosed heart failure
+def get_masks_list(dataset, feature_to_find):
+    """
+    Get list of masks with given feature in the prompt
+    """
+    samples_list = dataset.get_samples_list()
+    masks_list = []
+    for sample in samples_list:
+        prompt = sample['prompt']
+        mask_filename = os.path.join(dataset.dataset_path, sample['source'])
+        if feature_to_find in prompt:
+            masks_list.append(mask_filename)
+
+    print("Found {} masks with feature {}".format(len(masks_list), feature_to_find))
+
+    return masks_list
+
+
+diagnosed_masks = get_masks_list(dataset, feature_to_find="heart failure")
+healthy_masks = get_masks_list(dataset, feature_to_find="healthy")
 
 
 def generate_prompt(features):
+    """
+    Create random prompt from given features
+    """
+
     prompt = ""
     for feature in features:
         chosen_value = random.choice(feature)
@@ -60,6 +76,10 @@ def generate_prompt(features):
 
 
 def process(input_image, prompt, a_prompt, n_prompt, num_samples, image_resolution, ddim_steps=20, guess_mode=False, strength=1, scale=9, seed=-1, eta=0):
+    """
+    Generate synthetic image from input mask and prompt
+    """
+
     with torch.no_grad():
         
         img = input_image
@@ -100,6 +120,10 @@ def process(input_image, prompt, a_prompt, n_prompt, num_samples, image_resoluti
 
 
 def generate_synthetic_copy(dataset_path, output_path=None, per_sample_multiplier=1):
+    """
+    Generate synthetic version of dataset with real prompts and corresponding masks
+    """
+
     out_dir = 'synthetic_dataset'
 
     if output_path is None:
@@ -129,11 +153,22 @@ def generate_synthetic_copy(dataset_path, output_path=None, per_sample_multiplie
 
 
 def generate_random_prompt_dataset(output_dir, n_samples = 10):
+    """
+    Generate dataset with random prompts and real masks from patients diagnosed heart failure
+    """
+
     os.makedirs(output_dir, exist_ok=True)
 
     for i in tqdm(range(n_samples)):
         prompt = generate_prompt(features)
-        mask = random.choice(diagnosed_masks)
+        if "healthy" in prompt:
+            mask = random.choice(healthy_masks)
+        else:
+            mask = random.choice(diagnosed_masks)
+
+        mask = cv2.imread(mask)
+        mask = cv2.cvtColor(mask, cv2.COLOR_BGR2RGB)
+        mask = mask.astype(np.float32) / 255.0
         mask = torch.from_numpy(mask)
 
         sample = process(mask, prompt, "", "", 1, mask.shape[1])[0]
@@ -142,8 +177,14 @@ def generate_random_prompt_dataset(output_dir, n_samples = 10):
         filename = f'{i}_{filename}.png'
         output_path = os.path.join(output_dir, filename)
         sample = cv2.cvtColor(sample, cv2.COLOR_RGB2BGR)
+        mask = cv2.cvtColor(mask.numpy() * 255.0, cv2.COLOR_RGB2BGR)
         cv2.imwrite(output_path, sample)
+        cv2.imwrite(output_path.replace(".png", "_mask.png"), mask)
+    
+    print("Generated random prompt dataset with {} samples".format(n_samples))
 
+
+# Select one of the methods for synthetic dataset generation
 
 generate_random_prompt_dataset("synthetic_dataset/random_dataset", 10)
 # generate_synthetic_copy(source_dataset_path)
